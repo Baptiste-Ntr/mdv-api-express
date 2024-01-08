d7678b0d531596ffc5af88423a667a8b0f99b779
Baptiste-Ntr <98717656+Baptiste-Ntr@users.noreply.github.com>
2024-01-08 14:37:12 +0100

    add get team + driver

---
+++ app.ts
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('/driver to get driver data, /team to get team data')
+})
+
+app.get('/driver', (req, res) => {
+    res.sendFile(__dirname + '/data/driver.json')
+})
+
+app.get('/team', (req, res) => {
+    res.sendFile(__dirname + '/data/team.json')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
