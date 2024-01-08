087fca86a205b7e169baf47cd16c439809083511
Baptiste-Ntr <98717656+Baptiste-Ntr@users.noreply.github.com>
2024-01-08 16:25:23 +0100

    Refactor driverInfo route to use TypeScript and update dependencies

--- routes/driverInfo.ts
+++ routes/driverInfo.ts
@@ -1,11 +1,14 @@
-module.exports = function (app) {
+import { Application, Request, Response } from 'express'
+
+
+module.exports = function (app: Application) {
 
     const fs = require('fs')
     const request = require('request')
     const path = require('path')
 
-    app.get('/driverinfo/:driverName', (req, res) => {
-        let driverName = req.params.driverName;
+    app.get('/driverinfo/:driverName', (req: Request, res: Response) => {
+        let driverName: string = req.params.driverName;
         driverName = driverName.charAt(0).toUpperCase() + driverName.slice(1);
 
         fs.readFile(path.join(__dirname, '..', 'data', 'driver.json'), 'utf8', (error, data) => {
@@ -14,10 +17,10 @@ module.exports = function (app) {
             } else {
                 const driversData = JSON.parse(data)
                 const drivers = driversData.drivers
-                const driverInfo = drivers.find(driver => driver.lastname === driverName)
+                const driverInfo = drivers.find((driver) => driver.lastname === driverName)
 
                 if (driverInfo) {
-                    request(`http://ergast.com/api/f1/drivers/${driverName}.json`, (error, response, body) => {
+                    request(`http://ergast.com/api/f1/drivers/${driverName}.json`, (error: any, response: any, body: any) => {
                         if (!error && response.statusCode === 200) {
                             const drivers = JSON.parse(body)
                             // res.send(drivers)
